8ee6e726efdcb5810071378b2e9930e0baccb82b
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2024-11-28 08:04:03 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,15 @@ import routes from './routes';
 import { AppDataSource } from './config/database';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger.json';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: `${process.env.FRONTEND_URL}${process.env.PORT_FRONT}`,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
